d5d7caa4186d588f08cd3c8a255363574ef3094c
ftambler <federicotambler@gmail.com>
2025-11-01 16:44:25 -0300

    add cognito

---
+++ lambdas/global/signUpHandler/app.py
@@ -0,0 +1,14 @@
+import os
+import boto3
+
+dynamodb = boto3.resource("dynamodb", region_name=os.environ['REGION'])
+table = dynamodb.Table(os.environ["TABLE_NAME"])
+
+def lambda_handler(event, context):
+    if event['triggerSource'] == 'PostConfirmation_ConfirmSignUp':
+        user_email = event['request']['userAttributes'].get('email')
+        user_name = event['userName']
+
+        table.put_item(Item= { "PK": f"USERS#{user_email}", "SK": "PROFILE", "Name": user_name, "Credits": 0 })
+
+    return event
