b543cd5bcd9f3b047c15d3e7ca81bbeabceb112a
ftambler <federicotambler@gmail.com>
2025-11-01 17:13:08 -0300

    fix username -> name

--- lambdas/global/signUpHandler/app.py
+++ lambdas/global/signUpHandler/app.py
@@ -5,9 +5,10 @@ dynamodb = boto3.resource("dynamodb", region_name=os.environ['REGION'])
 table = dynamodb.Table(os.environ["TABLE_NAME"])
 
 def lambda_handler(event, context):
+    print(f"Received {event}")
     if event['triggerSource'] == 'PostConfirmation_ConfirmSignUp':
         user_email = event['request']['userAttributes'].get('email')
-        user_name = event.get('userName', 'notfound')
+        user_name = event.get('name', 'notfound')
 
         table.put_item(Item= { "PK": f"USERS#{user_email}", "SK": "PROFILE", "Name": user_name, "Credits": 0 })
 
