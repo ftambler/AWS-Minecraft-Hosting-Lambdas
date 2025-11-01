4e68e5cffa0d6b4b4602ada0ceddb135db676fa4
ftambler <federicotambler@gmail.com>
2025-11-01 16:45:50 -0300

    default user name

--- lambdas/global/signUpHandler/app.py
+++ lambdas/global/signUpHandler/app.py
@@ -7,7 +7,7 @@ table = dynamodb.Table(os.environ["TABLE_NAME"])
 def lambda_handler(event, context):
     if event['triggerSource'] == 'PostConfirmation_ConfirmSignUp':
         user_email = event['request']['userAttributes'].get('email')
-        user_name = event['userName']
+        user_name = event.get('userName', 'notfound')
 
         table.put_item(Item= { "PK": f"USERS#{user_email}", "SK": "PROFILE", "Name": user_name, "Credits": 0 })
 
